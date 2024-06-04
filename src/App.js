3ae7970010f7267443bd07b741daaf9a140cdc73
“Foxshane997” <Foxshane997@gmail.com>
2024-06-03 20:32:55 -0700

    Wokring on adding all components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@
 
 // Import components
 import './App.css';
+import Searchbar from './components/Searchbar';
+import Gallery from './components/Gallery';
+import Directory from './components/Directory';
+
 
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
+        <Searchbar/>
+        <Directory/>
+        <Gallery />
+        <sidebar/>
         {/* Your content will go here! */}
       </div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Help from "./Help";
+
+function Sidebar(){
+    return (
+        <div className="sideBar">
+            <Help/>
+        </div>
+    )
+}
